f61f0a2e630a3237eaa54096ecc7822703d39aac
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-27 15:36:25 -0600

    added orm

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var connection = require('./connection.js');
+
+// mysql connection
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('connected as id ' + connection.threadId);
+});
