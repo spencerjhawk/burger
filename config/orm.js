96ce570f3801f58d6e406e35dccb0ecd09553a4a
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-27 16:45:40 -0600

    fixed orm bug

--- config/orm.js
+++ config/orm.js
@@ -9,4 +9,61 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+var orm = {
+	// returns all table entries
+	selectAll: function(tableInput, cb) {
+		var queryString = "SELECT * FROM " + tableInput + ";";
+
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+
+	// inserts single table entry
+	insertOne: function(table, cols, vals, cb) {
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			// callback results
+			cb(result);
+		});
+	},
+
+	// updates single table entry
+	updateOne: function(table, objColVals, condition, cb) {
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			// callback results
+			cb(result);
+		});
+	}
+};
+
 module.exports = orm;
