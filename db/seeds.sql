26e6368c053b29f7582fa805103947a0bde02f12
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-27 01:25:00 -0600

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured, date)
+
+VALUES ("Deluxe Burger", false, "2017-11-26 05:00:10"),
+  ("Large Burger", false, "2017-11-26 05:00:20"),
+  ("Messy Burger", false, "2017-11-26 05:00:30"),
+  ("Funny Burger", false, "2017-11-26 05:00:40"),
+  ("The Biggest One We Have", false, "2017-11-26 05:00:50"),
+  ("Just The Bun, Thanks", false, "2017-11-26 05:01:00"),
+  ("Very Small Burger", true, "2017-11-26 05:01:10");
