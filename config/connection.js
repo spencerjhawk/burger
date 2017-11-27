b417ae10a10aaee27f11d93f5b9bed91bb82652d
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-27 15:32:25 -0600

    added config

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'burgers_db' // 
+  });
+}
+
+
+// export connection
+module.exports = connection;
